08c3b5f4fcca7dfd4ff2ba30998fad568471acfd
Ryan Potter <Ryan Potter>
2023-11-25 13:00:09 +0000

    Completed helper function to format seconds into 'HH:MM:SS'.

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,14 @@
+#include <cmath>
 #include <string>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  int hh = seconds / 3600;
+  int mm = (seconds % 3600) / 60;
+  int ss = seconds - hh * 3600 - mm * 60;
+  return std::to_string(hh) + ':' + std::to_string(mm) + ':' +
+         std::to_string(ss);
+}
