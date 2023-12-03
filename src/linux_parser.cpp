f209eae37fe8e17e7a7cdef039d0561c4c1fc8a9
Ryan Potter <Ryan Potter>
2023-12-03 10:45:38 +0000

    Added the uptime function to the linux parser namespace.

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -258,4 +258,14 @@ string LinuxParser::User(int pid) {
 
 // TODO: Read and return the uptime of a process
 // REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::UpTime(int pid[[maybe_unused]]) { return 0; }
+long LinuxParser::UpTime(int pid) {
+  string line, pid, comm, state, ppid, pgrp, session, tty_nr, tpgid, flags, minflt, cminflt, majflt, cmajflt, utime;
+  std::ifstream filestream(kProcDirectory + '/' + to_string(pid) + kStatFilename);
+  if(filestream.is_open()){
+    std::getline(filestream,line);
+    std::istringstream linestream(line);
+    linestream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt >> utime;
+    return std::stol(utime)/sysconf(_SC_CLK_TCK);
+  }
+  return 0;
+}
